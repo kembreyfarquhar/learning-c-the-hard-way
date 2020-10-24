a6cf70902a8f3cb6dbd70d994eb23f2f6fc15ca2
kmcknight1 <katiemcknight1991@gmail.com>
2020-10-24 18:36:32 -0500

    tweaks and fixes

--- ex16/extracredit.c
+++ ex16/extracredit.c
@@ -22,18 +22,21 @@ struct Person Person_create(char *name, int age, int height)
 }
 
 void Person_destroy(struct Person *who){
+	free(who->name);
 	who = NULL;
 }
 
 void Person_print(struct Person who){
-	printf("Name: %s\n", who.name);
-	printf("Age: %d\n", who.age);
-	printf("Height: %d\n", who.height);
+	printf("\nPERSON STRUCT CREATED:\n      {\n");
+	printf("\tName: %s\n", who.name);
+	printf("\tAge: %d\n", who.age);
+	printf("\tHeight: %d\n", who.height);
+	printf("      }\n");
 }
 
 char *clean_name(char *name, int length)
 {
-	char *name_clean = malloc(length + 1);
+	char *name_clean = malloc(length * sizeof(char));
 	for(int i = 0; i < length; i++){
 		if(i == length - 1){
 			name_clean[i] = '\0';
@@ -57,6 +60,7 @@ int main(int argc, char *argv[])
 	scanf("%d", &height);
 	char *n = clean_name(name, strlen(name));
 	struct Person input = Person_create(n, age, height);
+	printf("input.name: %s\ninput.name Address: %p\n", input.name, input.name);
 	Person_print(input);
 	Person_destroy(&input);
 	
